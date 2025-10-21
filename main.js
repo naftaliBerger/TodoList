237e92a061145ce9d26ccad990099400850672a0
Naftali Berger <bn26665@gmail.com>
2025-10-21 15:41:46 +0300

    Completion of 2 parts of phase B

--- main.js
+++ main.js
@@ -3,34 +3,51 @@ var input = document.querySelector("input");
 var taskContainer = document.createElement("div");
 document.body.appendChild(taskContainer);
 var doneContainer = null;
+// const deletAll = document.createElement("buttuo");
+//   deletAll.classList = "Btn";
+//   deletAll.textContent = "🗑️";
+//   deletAll.onclick = () =>{
+//     taskContainer.remove();
+//   }
+//   taskContainer.appendChild(deletAll)
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     if (!input.value)
         return;
     var task = document.createElement("div");
+    task.id = "task";
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     var delet = document.createElement("button");
-    delet.id = "deletBtn";
+    delet.classList = "Btn";
     delet.textContent = "❌";
     delet.onclick = function () {
         task.remove();
     };
-    var taskInput = document.createElement("div");
+    var taskInput = document.createElement("p");
     taskInput.textContent = input.value;
+    var OldTask = taskInput.textContent;
     var edit = document.createElement("button");
+    edit.classList = "Btn";
     edit.textContent = "✏️";
     edit.onclick = function () {
         taskInput.setAttribute("contentEditable", "true");
         taskInput.focus();
+        taskInput.addEventListener("keydown", function (e) {
+            if (e.key === "Enter") {
+                taskInput.setAttribute("contentEditable", "false");
+            }
+            else if (e.key === "Escape") {
+                taskInput.textContent = OldTask;
+                taskInput.setAttribute("contentEditable", "false");
+            }
+        });
     };
     task.append(checkbox, taskInput, delet, edit);
     taskContainer.appendChild(task);
     checkbox.addEventListener("change", function () {
         if (checkbox.checked) {
-            console.log('chek');
             if (!doneContainer) {
-                console.log('ghjkl');
                 doneContainer = document.createElement("div");
                 doneContainer.appendChild(document.createElement("hr"));
                 var title = document.createElement("h2");
@@ -42,7 +59,6 @@ form.addEventListener("submit", function (e) {
         }
         else {
             taskContainer.appendChild(task);
-            console.log('fghjkl');
         }
     });
     input.value = "";
--- main.ts
+++ main.ts
@@ -1,57 +1,72 @@
 const form = document.querySelector("form")!;
 const input = document.querySelector("input")!;
 const taskContainer = document.createElement("div");
-document.body.appendChild(taskContainer); 
+document.body.appendChild(taskContainer);
 
-let doneContainer: HTMLDivElement | null = null; 
+let doneContainer: HTMLDivElement | null = null;
+
+// const deletAll = document.createElement("buttuo");
+//   deletAll.classList = "Btn";
+//   deletAll.textContent = "🗑️";
+//   deletAll.onclick = () =>{
+//     taskContainer.remove();
+//   }
+//   taskContainer.appendChild(deletAll)
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (!input.value) return;
-    
-    const task = document.createElement("div");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    const delet = document.createElement("button");
-    delet.id = "deletBtn";
-    delet.textContent = "❌";
-    delet.onclick = () => {
-        task.remove();
-    }
-  let taskInput = document.createElement("div");
+  e.preventDefault();
+  if (!input.value) return;
+
+  const task = document.createElement("div");
+  task.id = "task";
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  const delet = document.createElement("button");
+  delet.classList = "Btn";
+  delet.textContent = "❌";
+  delet.onclick = () => {
+    task.remove();
+  };
+
+  let taskInput = document.createElement("p");
   taskInput.textContent = input.value;
+  const OldTask = taskInput.textContent;
   const edit = document.createElement("button");
+  edit.classList = "Btn";
   edit.textContent = "✏️";
   edit.onclick = () => {
     taskInput.setAttribute("contentEditable", "true");
     taskInput.focus();
-  }
-  task.append(checkbox, taskInput, delet, edit);
-  
-  taskContainer.appendChild(task);
 
+    taskInput.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        taskInput.setAttribute("contentEditable", "false");
+      } else if (e.key === "Escape") {
+        taskInput.textContent = OldTask;
+        taskInput.setAttribute("contentEditable", "false");
+      }
+    });
+  };
+  task.append(checkbox, taskInput, delet, edit);
+
+  taskContainer.appendChild(task);
 
   checkbox.addEventListener("change", () => {
     if (checkbox.checked) {
-        console.log('chek');
-        
       if (!doneContainer) {
-        console.log('ghjkl');
-        
         doneContainer = document.createElement("div");
         doneContainer.appendChild(document.createElement("hr"));
         const title = document.createElement("h2");
         title.textContent = "Tasks done";
+
+        
         doneContainer.appendChild(title);
         document.body.appendChild(doneContainer);
       }
-      
-      doneContainer.appendChild(task);
-    }
-     else {
 
-       taskContainer.appendChild(task);
-      console.log('fghjkl');
-      
+      doneContainer.appendChild(task);
+    } else {
+      taskContainer.appendChild(task);
     }
   });
 
